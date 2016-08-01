eb453b5947a5d57fbb750ee85ee7c533c0d8326d
Dave Woods <dwoodscs@gmail.com>
2016-08-01 15:55:58 +0000

    Added basic handlebars functionality

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
 const port = process.env.PORT || 8080;
 const path = require('path');
 
-app.get('/', function(req, res) {
-	//res.sendFile(path.join(__dirname + '/index.html'));
-	res.send('Hello person.');
-});
-
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 app.use('/public', express.static('public'));
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
 
 app.listen(port, function(err) {
     if (err) throw err;
