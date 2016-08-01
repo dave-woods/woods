7c381817e453be0a6db89c93341eb5958c2d9f75
Dave Woods <dwoodscs@gmail.com>
2016-08-01 17:31:10 +0000

    Added database with name values and query route

--- app.js
+++ app.js
@@ -3,20 +3,84 @@ const exphbs = require('express-handlebars');
 const app = express();
 const port = process.env.PORT || 8080;
 const path = require('path');
+const fs = require('fs');
+
+// var c = 0;
+// lineReader.on('line', function(line) {
+//     if (c < 10)
+//         console.log('Line from file: ' + line);
+//     c = c + 1;
+// });
 
 app.engine('hbs', exphbs({
     defaultLayout: 'main',
     extname: 'hbs',
     layoutsDir: path.join(__dirname, 'views/layouts')
 }));
-
 app.set('view engine', 'hbs');
-
 app.use('/public', express.static('public'));
+
 
 app.get('/', function(req, res) {
 	res.render('index', {title: 'woodsapp'});
 });
+
+app.get('/namegen', function(req, res) {
+    const rand = Math.floor(Math.random() * 708); // number of entries in db, could be retrieved with db.count()
+    const mongo = require('mongodb').MongoClient;
+    mongo.connect(process.env.MONGOLAB_URI, function (err, db) {
+        if (err)
+            console.log('Unable to connect to the mongoDB server. Error:', err);
+        else
+        {
+            db.collection('projectNames')
+                .find()
+                .limit(-1)
+                .skip(rand)
+                .next()
+                .then(function(result) {
+                    db.close();
+                    //console.log(result);
+                    res.json({id: rand, name: result.name, hasSpace: result.hasSpace});
+                }, function(err) {
+                    db.close();
+                    throw err;
+                });
+        }
+    });
+});
+
+// app.get('/namegen/add', function(req, res) {
+//     const mongo = require('mongodb').MongoClient;
+//     mongo.connect(process.env.MONGOLAB_URI, function (err, db) {
+//         if (err)
+//             console.log('Unable to connect to the mongoDB server. Error:', err);
+//         else
+//         {
+//             const lineReader = require('readline').createInterface({
+//                 input: fs.createReadStream('namegen/list')
+//             });
+//             lineReader.on('line', function(line, last) {
+//                 const document = {name: line, hasSpace: (line.indexOf(' ') >= 0)};
+//                 if (!last)
+//                 {
+//                     db.collection('projectNames').insert(document, function(err, data) {
+//                         if (err) throw err;
+//                     });
+//                 }
+//                 else
+//                 {
+//                     db.collection('projectNames').insert(document, function(err, data) {
+//                         if (err) throw err;
+//                         db.close();
+//                         res.end('done');
+//                     });
+//                 }
+//                 console.log(line);
+//             });
+//         }
+//     });
+// });
 
 app.get('/about', function(req, res) {
     res.render('about', {title: 'woodsapp'});
@@ -25,6 +89,7 @@ app.get('/about', function(req, res) {
 app.get('/hello/:name', function(req, res) {
     res.render('hello', {title: 'woodsapp', name: req.params.name});
 });
+
 
 app.listen(port, function(err) {
     if (err) throw err;
