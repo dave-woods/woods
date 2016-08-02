bbda28e2222855bdda440b042a0967b383517590
Dave Woods <dwoodscs@gmail.com>
2016-08-02 00:59:33 +0000

    Added some front-end features

--- app.js
+++ app.js
@@ -3,14 +3,8 @@ const exphbs = require('express-handlebars');
 const app = express();
 const port = process.env.PORT || 8080;
 const path = require('path');
-const fs = require('fs');
 
-// var c = 0;
-// lineReader.on('line', function(line) {
-//     if (c < 10)
-//         console.log('Line from file: ' + line);
-//     c = c + 1;
-// });
+const appTitle = 'WoodsApp';
 
 app.engine('hbs', exphbs({
     defaultLayout: 'main',
@@ -19,14 +13,13 @@ app.engine('hbs', exphbs({
 }));
 app.set('view engine', 'hbs');
 app.use('/public', express.static('public'));
-
+app.use('/css', express.static('css'));
 
 app.get('/', function(req, res) {
-	res.render('index', {title: 'woodsapp'});
+	res.render('index', {title: appTitle});
 });
 
 app.get('/namegen', function(req, res) {
-    
     var allowSpace = undefined;
     if (req.query.allowSpace && req.query.allowSpace.length > 0 && (req.query.allowSpace == 'false' || req.query.allowSpace == 0))
         allowSpace = {hasSpace: false};
@@ -54,46 +47,13 @@ app.get('/namegen', function(req, res) {
     });
 });
 
-// app.get('/namegen/add', function(req, res) {
-//     const mongo = require('mongodb').MongoClient;
-//     mongo.connect(process.env.MONGOLAB_URI, function (err, db) {
-//         if (err)
-//             console.log('Unable to connect to the mongoDB server. Error:', err);
-//         else
-//         {
-//             const lineReader = require('readline').createInterface({
-//                 input: fs.createReadStream('namegen/list')
-//             });
-//             lineReader.on('line', function(line, last) {
-//                 const document = {name: line, hasSpace: (line.indexOf(' ') >= 0)};
-//                 if (!last)
-//                 {
-//                     db.collection('projectNames').insert(document, function(err, data) {
-//                         if (err) throw err;
-//                     });
-//                 }
-//                 else
-//                 {
-//                     db.collection('projectNames').insert(document, function(err, data) {
-//                         if (err) throw err;
-//                         db.close();
-//                         res.end('done');
-//                     });
-//                 }
-//                 console.log(line);
-//             });
-//         }
-//     });
-// });
-
 app.get('/about', function(req, res) {
-    res.render('about', {title: 'woodsapp'});
+    res.render('about', {title: appTitle});
 });
 
 app.get('/hello/:name', function(req, res) {
-    res.render('hello', {title: 'woodsapp', name: req.params.name});
+    res.render('hello', {title: appTitle, name: req.params.name});
 });
-
 
 app.listen(port, function(err) {
     if (err) throw err;
